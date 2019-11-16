e1b6dd3cd5e5a71a3378d41ba3d3a5650a11a2f7
hyunsung <hslgood@gmail.com>
2019-11-16 16:17:39 +0900

    part-one/src/functional-shared/stack.js complete by hs, 191116

--- part-one/src/functional-shared/stack.js
+++ part-one/src/functional-shared/stack.js
@@ -1,9 +1,48 @@
 const Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  // 숫자 키가 있는 객체를 사용하여 값 저장
+  someInstance.storage = {};
+  someInstance.count = 0;
+  
+  extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 const stackMethods = {};
+
+// Implement the methods below
+stackMethods.push = function (value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+stackMethods.pop = function () {
+  // Remove and return the string on the top of the stack
+  if (this.count === 0) {
+    return "stack is empty"
+  }
+
+  var popTarget = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return popTarget;
+}
+
+stackMethods.size = function () {
+  //Return the number of items on the stack
+  return this.count;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key]
+  }
+};
+
 
 if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
