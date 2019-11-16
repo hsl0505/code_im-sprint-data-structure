9d3cc80493b7e98a689fc4ab9fb99ffaf1996315
hyunsung <hslgood@gmail.com>
2019-11-16 15:43:37 +0900

    functional queue changed by hs, 191116

--- part-one/src/functional/queue.js
+++ part-one/src/functional/queue.js
@@ -2,33 +2,40 @@ const Queue = function () {
   const someInstance = {};
 
 
-  //npm run test:part-one  
+  //npm run test:part-one
   // Use an object with numeric keys to store values
-  const storage = {};
-  var front = 0; 
-  var back = 0;
+  someInstance.storage = {};
+  someInstance.count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function (value) {
   // Add a string to the back of the queue
   // 대기열 뒷면에 문자열 추가
-  storage[back] = value;
-  back++;
-};
+  someInstance.count++;
+  someInstance.storage[someInstance.count] = value;
+  };
   someInstance.dequeue = function () {
   //Remove and return the string at the front of the queue
   //줄 앞의 줄을 제거하고 반환하십시오.
-  delete storage[front];
-  var frontNum = storage[front];
-  front++;
-  return frontNum;
-};
+  if (someInstance.count === 0) {
+    return "queue is empty"
+  }
+  
+  var dequeueTarget = someInstance.storage["1"];
+  delete someInstance.storage["1"];
+  someInstance.count--;
+  for (var i=1; i<=someInstance.count; i=i+1) {
+    someInstance.storage[`${i}`] = someInstance.storage[`${i+1}`];
+  }
+  delete someInstance.storage[`${someInstance.count + 1}`];
+  return dequeueTarget;
+  };
   someInstance.size = function () {
   //Return the number of items in the queue
   //대기열에있는 항목 수를 반환
-  return back;
-};
+  return someInstance.count;
+  };
   return someInstance;
 };
 
