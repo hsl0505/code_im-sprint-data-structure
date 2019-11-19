4b2aeccaee4bf3088971e158f8df68a41092c48e
hyunsung <hslgood@gmail.com>
2019-11-19 17:30:29 +0900

    change

--- part-two/src/binarySearchTree.js
+++ part-two/src/binarySearchTree.js
@@ -2,14 +2,14 @@ const BinarySearchTree = function(value) {
     // functional 방식으로 구현
     var tree = {};
     tree.value = value;
-    tree.left = {};
-    tree.left.childNode = null;
-    tree.right = {};
-    tree.right.childNode = null;
+    tree.left = null;
+    //tree.left.childNode = null;
+    tree.right = null;
+    //tree.right.childNode = null;
 
     tree.insert = function(value) {   // 5 -> 2 -> 3 -> 7 -> 6
       if (value > tree.value) {
-          if (tree.right.childNode === null) {
+          if (tree.right === null) {
             tree.right = BinarySearchTree(value);
           }
           else {
@@ -17,7 +17,7 @@ const BinarySearchTree = function(value) {
           }
       }
       else if (value < tree.value) {
-          if (tree.left.childNode === null) {
+          if (tree.left === null) {
             tree.left = BinarySearchTree(value);
           }
           else {
@@ -30,39 +30,52 @@ const BinarySearchTree = function(value) {
     };
 
     tree.contains = function(target) {
+      
         var containTarget = tree;
+        
         var result = false;
 
         function recursion(target, recursionTarget) {
-          if (target === recursionTarget.value) {
+          if (target === recursionTarget.value) { // tree.value
               result = true;
           }
-          else if (target > recursionTarget.value) {
+          else if (target > recursionTarget.value ) {
+            if (recursionTarget.right !== null) {
               recursionTarget = recursionTarget.right;
               recursion(target, recursionTarget)
+            }
           }
           else if (target < recursionTarget.value) {
+            if (recursionTarget.left !== null) {
               recursionTarget = recursionTarget.left
               recursion(target, recursionTarget)
+            }
           }
         }
         
         recursion(target, containTarget)
         return result;
+        
     };
 
     tree.depthFirstLog = function(callback) {
+      
       var depthTarget = tree;
       
       function recursion(target) { 
           callback(target.value)
           
-          var targetLeft = target.left.childNode;
-          recursion(targetLeft)
-          
+          if (target.right !== null) {
+            target = target.right
+            recursion(target)
+          }
+          if (target.left !== null) {
+            target = target.left
+            recursion(target)
+          }
       }
-
       recursion(depthTarget)
+      
     };
 
     return tree;
--- part-two/src/hashTable.js
+++ part-two/src/hashTable.js
@@ -5,16 +5,21 @@ const HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  const index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(k, v) { // 스티븐, 시갈
+  const index = getIndexBelowMaxForKey(k, this._limit); // 임의의 숫자(책갈피)
+  this._storage.set(index, v)
 };
 
 HashTable.prototype.retrieve = function(k) {
   const index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)
 };
 
 HashTable.prototype.remove = function(k) {
   const index = getIndexBelowMaxForKey(k, this._limit);
+  if(index) {
+    return index;
+  }
 };
 
 /*
